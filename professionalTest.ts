91c3f5b3f11bf6a52a0c41200d973f6444d7d04d
Manuela-Mariscotti <you@example.com>
2022-07-23 18:05:30 +0200

    test de objeto professional

--- professionalTest.ts
+++ professionalTest.ts
@@ -1,5 +1,17 @@
 import { Professional } from "./professional";
 
-let AlPacino = new Professional ("Al Pacino",82,"male",85,1.70,"black","black","white",false,"american",1,"actor");
+let AlPacino = new Professional ("Al Pacino",82,"male",85,1.70,"black","brown","white",false,"american",1,"actor, director, productor, guionista");
+
+let RoberDeNiro = new Professional ("Rober De Niro",78,"male",90,1.72,"grey","brown","white",false,"american",2,"actor, director, productor, guionista ");
+
+let LeoDicaprio = new Professional ("Leonardo DiCaprio",47,"male",89,1.83,"blond","cyan","white",false,"american",1,"actor,productor de cine");
+
+let QuentinTarantino = new Professional ("Quentin Tarantino",59,"male",85,1.82,"brown","brown","white",false,"american",1,"director, productor, guinista, actor");
 
 AlPacino.print();
+
+RoberDeNiro.print();
+
+LeoDicaprio.print();
+
+QuentinTarantino.print();
