7142a0662b26a447f06347fd5684bf8bacca6353
Manuela-Mariscotti <you@example.com>
2022-07-23 18:12:49 +0200

    clase professionals y su test

---
+++ professional.js
@@ -0,0 +1,35 @@
+"use strict";
+exports.__esModule = true;
+exports.Professional = void 0;
+var Professional = /** @class */ (function () {
+    function Professional(name, age, genre, weight, height, hairColor, eyeColor, race, isRetired, nacionality, oscarNumber, profession) {
+        this.name = name;
+        this.age = age;
+        this.genre = genre;
+        this.weight = weight;
+        this.height = height;
+        this.hairColor = hairColor;
+        this.eyeColor = eyeColor;
+        this.race = race;
+        this.isRetired = isRetired;
+        this.nacionality = nacionality;
+        this.oscarNumber = oscarNumber;
+        this.profession = profession;
+    }
+    Professional.prototype.print = function () {
+        console.log("Name : ".concat(this.name));
+        console.log("Age : ".concat(this.age));
+        console.log("Genre : ".concat(this.genre));
+        console.log("Weight : ".concat(this.weight));
+        console.log("Height : ".concat(this.height));
+        console.log("Hair color : ".concat(this.hairColor));
+        console.log("Eye color: ".concat(this.eyeColor));
+        console.log("Race : ".concat(this.race));
+        console.log("Is retired? : ".concat(this.isRetired));
+        console.log("Nacionality : ".concat(this.nacionality));
+        console.log("Oscar numbers : ".concat(this.oscarNumber));
+        console.log("Profession : ".concat(this.profession));
+    };
+    return Professional;
+}());
+exports.Professional = Professional;
---
+++ professionalTest.js
@@ -0,0 +1,9 @@
+"use strict";
+exports.__esModule = true;
+var professional_1 = require("./professional");
+var AlPacino = new professional_1.Professional("Al Pacino", 82, "male", 85, 1.70, "black", "black", "white", false, "american", 1, "actor, director, productor, guionista");
+var RoberDeNiro = new professional_1.Professional("Rober De Niro", 78, "male", 90, 1.72, "grey", "brown", "white", false, "american", 2, "actor, director, productor, guionista ");
+var LeoDicaprio = new professional_1.Professional("Leonardo DiCaprio", 47, "male", 89, 1.83, "blond", "cyan", "white", false, "american", 1, "actor,productor de cine");
+AlPacino.print();
+RoberDeNiro.print();
+LeoDicaprio.print();
