994a40f0413aad5ed9d9b8cd4c5108ce8ef5fd0c
DianaJimenez09 <djimenezneculman@gmail.com>
2022-07-26 11:47:22 +0200

    movieTest

--- movieTest.ts
+++ movieTest.ts
@@ -1,14 +1,17 @@
 import { Movie } from "./movie";
 
-get Professional()
+let miActors = new Movie("Rec",2007,"española","terror")
+console.log(miActors.getActors)
 
-let rec = new Movie ("Rec",2007,"española","terror",);
+
+let rec = new Movie ("Rec",2007,"española","terror");
 
 let interestellar = new Movie ("Interestellar",2014,"britanico-estadounidense","ciencia ficcion");
 
 let rogueOne = new Movie ("Rogue One",2016,"estadounidense","ciencia ficcion");
 
 let unTriomphe = new Movie ("El triunfo",2020,"francesa","comedia");
+
 
 rec.print();
 interestellar.print();
