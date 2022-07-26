6710f33281859f0aedb75d1dbc83a3f27c6e862a
DianaJimenez09 <djimenezneculman@gmail.com>
2022-07-26 10:11:56 +0200

    archivo movie

--- movie.ts
+++ movie.ts
@@ -1,4 +1,4 @@
-import { Professional } from "./professional";
+import { Professional } from "./professional"
 
 export class Movie{
     public title:string;
@@ -14,28 +14,26 @@ export class Movie{
     public producer:string;
     public distributor:string;
     public genre:string;
+    public profesionals: Professional[];
 
-    constructor (title:string,releaseYear:number,nacionality:string,genre:string){
+    constructor (title:string,releaseYear:number,nacionality:string,genre:string,profesionals:[]){
         this.title = title;
         this.releaseYear = releaseYear;
         this.nacionality = nacionality;
         this.genre = genre;
+        this.profesionals = []
     }
 
 
-    public getTitle():string{
-        return this.title
-    };
+    
 
-    public getReleaseYear():number{
-        return this.releaseYear
-    };
+    public print():void{
+        console.log(`Title : ${this.title}`);
+        console.log(`ReleaseYear : ${this.releaseYear}`);
+        console.log(`Nacionality : ${this.nacionality}`);
+        console.log(`Genre : ${this.genre}`);
     
-    public getNacionality():string{
-        return this.nacionality
-    };
-    
-    public getGenre():string{
-        return this.genre
     }
+   
+
 }
