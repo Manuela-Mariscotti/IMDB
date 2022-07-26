d01d041e87c48818fede43c33a68906b76e2b123
Manuela-Mariscotti <manuela.mariscotti@gmail.com>
2022-07-26 16:05:36 +0200

    convertir a JSON

--- imdbTest.ts
+++ imdbTest.ts
@@ -4,5 +4,9 @@ import { movies1 } from "./movieTest";
 
 let imdb1 = new Imdb (movies1);
 
-imdb1.peliculas[0].printMovie();
-imdb1.peliculas[1].printMovie();
+//imdb1.peliculas[0].printMovie();
+//imdb1.peliculas[1].printMovie();
+
+let data = JSON.stringify(imdb1);
+import fs from 'fs';
+fs.writeFileSync("imdbBBDD.json",data);
